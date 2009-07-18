c4a5b188b6c578c245d9936c38e96d30e863c6de
Benjamin Turner <benturn@gmail.com>
2009-07-18 13:38:06 -0500

    can now start or suspend from select options

--- vm.sh
+++ vm.sh
@@ -7,18 +7,47 @@
 # * Show running VMs and enable stopping
 # * Check VMWare exists and exit if it doesn't (cross-platform)
 
+startflag=
+pauseflag=
+stopflag=
+while getopts 'sp' OPTION
+do
+  case $OPTION in
+  s)  startflag=1
+    ;;
+  p)  pauseflag=1
+    ;;
+  ?)  printf "Usage %s: [-s] [-p] args\n" $(basename $0) >&2
+    exit 2
+    ;;
+  esac
+done
+
 VM_DIR="${HOME}/Documents/Virtual Machines.localized"
-VM_COMMAND="/Library/Application Support/VMware Fusion/vmrun"
-VM_LIST=`find "${VM_DIR}" -name "*.vmwarevm" | grep -v 'Win' | grep -v 'XP'`
+VMRUN="/Library/Application Support/VMware Fusion/vmrun"
+
+if [ "$startflag" ]
+then
+  VM_LIST=`find "${VM_DIR}" -name "*.vmwarevm" | grep -v 'Win' | grep -v 'XP'`
+  VM_COMMAND="start"
+  EXTRA="nogui"
+fi
+
+if [ "$pauseflag" ]
+then
+  VM_LIST=`"${VMRUN}" list`
+  VM_COMMAND="suspend"
+  EXTRA="soft"
+fi
 
 # path separators so I can use spaces in filenames
 OLD_IFS=${IFS}; IFS="
 "
 
-echo "Choose a non-Windows VM to start:"
+echo "Choose a non-Windows VM to $VM_COMMAND:"
 select opt in $VM_LIST ; do
     # run file
-    `"${VM_COMMAND}" start "${opt}" nogui`
+    `"${VMRUN}" "$VM_COMMAND" "${opt}" "${EXTRA}"`
     # repair IFS
     IFS=${OLD_IFS}
     exit 0
